0fb8cf32871d7244c6c8c994e6b5242d8fcaf9d1
phatIntern <phat.pham@saleshood.com>
2022-08-30 08:08:38 +0700

    reproduce the antd icon error on notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,62 @@
 import { useState } from "react";
-import { Trans, useTranslation } from "react-i18next";
-import CloseOutlined from "@ant-design/icons/CloseOutlined";
+import "antd/dist/antd.css";
+import "./ds-index.css";
 import "./i18n/init";
 import "./App.css";
+import { Modal, Button, ConfigProvider, notification } from "antd";
+
+ConfigProvider.config({
+  prefixCls: "ds",
+  iconPrefixCls: "dsicon",
+});
 
 function App() {
-  const [count, setCount] = useState(0);
-  const { t: buyerSitesT} = useTranslation('buyer-sites');
-  const infoMessage = (
-    <Trans
-      t={buyerSitesT}
-      i18nKey='SHARE_VIA_BUYER_SITE_DIALOG.NOTIFICATION.INFO_SHARED_MESSAGE'
-      values={{
-        count: 1,
-        BS_NAME: "Umberella",
-      }}
-    />
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <ConfigProvider prefixCls="ds" iconPrefixCls="dsicon">
+        <div style={{
+          marginBottom: 8,
+        }}>
+        <Button Button type="primary" onClick={() => {
+          notification.success({
+            message: 'Success',
+            description: 'Notice the close icon'
+          })
+        }}>
+          Open static notification
+        </Button>
+        </div>
+
+        <Button type="primary" onClick={showModal}>
+          Open Modal
+        </Button>
+        <Modal
+          title="Basic Modal"
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </ConfigProvider>
+    </>
   );
-
-  return (<div>
-    {infoMessage}
-    <div>
-    <button onClick={() => setCount(count +1)} >
-      +
-    </button>
-    </div>
-
-  </div>)
 }
 
 export default App;
