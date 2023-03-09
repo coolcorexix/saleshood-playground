b0e3f16090917d8067ba2dec2a95526bc08e2f6c
phatIntern <phat.pham@saleshood.com>
2023-03-09 15:01:01 +0700

    minimal reproduce

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,99 @@
-import { useState } from "react";
-import { Trans, useTranslation } from "react-i18next";
-import CloseOutlined from "@ant-design/icons/CloseOutlined";
-import "./i18n/init";
+import React from 'react'
 import "./App.css";
+import { Menu, Typography } from 'antd';
 
-function App() {
-  const [count, setCount] = useState(0);
-  const { t: buyerSitesT} = useTranslation('buyer-sites');
-  const infoMessage = (
-    <Trans
-      t={buyerSitesT}
-      i18nKey='SHARE_VIA_BUYER_SITE_DIALOG.NOTIFICATION.INFO_SHARED_MESSAGE'
-      values={{
-        count: 1,
-        BS_NAME: "Umberella",
-      }}
+
+export const SomeContext = React.createContext();
+export const SomeProvider = SomeContext.Provider;
+
+import { MailOutlined } from '@ant-design/icons';
+
+function getItem(
+  label,
+  key,
+  icon,
+  children,
+  type,
+) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem(
+      'Item 1',
+      'g1',
+      null,
+      [
+        getItem(<Typography.Text ellipsis>Option 1sssssssssssssssssssssssssssssss</Typography.Text>, '1'),
+        getItem('Option 2', '2'),
+      ],
+      'group'
+    ),
+    getItem(
+      'Item 2',
+      'g2',
+      null,
+      [getItem('Option 3', '3'), getItem('Option 4', '4')],
+      'group'
+    ),
+  ]),
+
+  getItem('Navigation Two', 'sub2', <MailOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Submenu', 'sub3', null, [
+      getItem('Option 7', '7'),
+      getItem('Option 8', '8'),
+    ]),
+  ]),
+
+  { type: 'divider' },
+
+  getItem('Navigation Three', 'sub4', <MailOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+
+  getItem(
+    'Group',
+    'grp',
+    null,
+    [getItem('Option 13', '13'), getItem('Option 14', '14')],
+    'group'
+  ),
+];
+
+export const Template2 = () => {
+  const onClick = (e) => {
+    console.log('click ', e);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      style={{ width: 256 }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode='inline'
+      items={items}
     />
   );
+};
 
-  return (<div>
-    {infoMessage}
-    <div>
-    <button onClick={() => setCount(count +1)} >
-      +
-    </button>
-    </div>
+function App() {
 
-  </div>)
+  return (
+    <Template2 />
+  );
 }
 
 export default App;
